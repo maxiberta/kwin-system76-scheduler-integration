093a269670275feaa240d02c712f1ec8b812fd80
Maximiliano Bertacchini <maxiberta@gmail.com>
2024-03-20 02:37:50 -0300

    Update to KWin Scripting API 6.0

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,3 +1,6 @@
-workspace.clientActivated.connect(function(client) {
+// See https://develop.kde.org/docs/plasma/kwin/api/
+
+workspace.windowActivated.connect(function(client) {
+    //console.log("CLIENT PID", client.pid);
     callDBus("com.system76.Scheduler", "/com/system76/Scheduler", "com.system76.Scheduler", "SetForegroundProcess", client.pid);
 })
